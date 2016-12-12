7c87fdc49bd0f502b12d63f27983383c5e5ee361
Rahul Maurya <rahulmaurya.mnnit@gmail.com>
2016-12-12 08:41:06 +0530

    stylying of about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,23 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component!</h3>
+    <div>
+      <h1 className="text-center"> About! </h1>
+      <p>
+        This is a weather application build on React. I have built this for Complete react web developer course.
+      </p>
+      <p>
+        Here are some of the tools that i used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was javascript framework used.
+        </li>
+        <li>
+          <a href="https://openweathermap.org">Open weather Map</a> - I used open weather map to get weather data by city name .
+        </li>
+      </ul>
+    </div>
   )
 };
 
